d9110c7d74f71c86619d7a4f8955a87d550272d8
jaikumar2406 <ankush654123@gmail.com>
2023-12-30 13:50:13 +0530

    first commit

---
+++ rungta/java.js
@@ -0,0 +1,14 @@
+
+ScrollReveal({
+    reset: true,
+    distance:'60px',
+    duration:2500,
+    delay:500,
+})
+ScrollReveal().reveal("#text" );
+ScrollReveal().reveal("#text2", {ease :" ease-in"}, { delay: 10000});
+ScrollReveal().reveal("#text3", {ease :" ease-in"}, { delay: 10000});
+
+
+
+
